61ddfddb796d8616c1f96c2cf7d08a719425f96f
Brian Montana <contact@brianmontana.net>
2018-10-09 19:05:25 -0500

    Updating terminal path for Adobe on Unix, need to add error handling with proper trouble shooting response

--- scripts/dist-path.js
+++ scripts/dist-path.js
@@ -3,12 +3,12 @@ const exec = require('child_process').exec;
 // Check platform
 // Determine how to build the path for dist, based on a generic ignored file
 if(process.platform === 'win32') {
-    process.env.npm_package_config_distPath = process.env.APPDATA + '\\Adobe\\CEP\\extensions\\com.cep-starter';
-  }
+  process.env.npm_package_config_distPath = process.env.APPDATA + '\\Adobe\\CEP\\extensions\\com.cep-starter';
+}
 
-  if(!fs.existsSync(process.env.npm_package_config_distPath)) {
-    console.error('Directory doesn\'t exist!');
-  }
+if(!fs.existsSync(process.env.npm_package_config_distPath)) {
+  console.error(`Directory doesn\'t exist! Can\'t write to ${process.env.npm_package_config_distPath}`);
+}
 
 exec(`ng build --output-path ${process.env.npm_package_config_distPath}`,
   function(err, stdout, stderr) {
