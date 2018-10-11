a40ed8c846f1e0df20a56c5d1abc30c5282371c9
Montana, Brian <bmontana@conversantmedia.com>
2018-10-11 15:01:39 -0500

    Removing unused files

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  development: false,
-  web: false,
+  production: true
 };
--- environments/environment.web.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  development: true,
-  web: true,
-};
